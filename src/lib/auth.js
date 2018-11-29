0716b7f848e43f9d348045f18ac50f1ea217fddf
weaklion <weaklion@naver.com>
2018-11-29 15:12:03 +0900

    spa commit

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const checkEmailExists = (email) => axios.get('/api/auth/exists/email/' + email);
-export const checkUsernameExists = (username) => axios.get('/api/auth/exists/username/' + username);
+export const checkEmailExists = (email) => axios.get('/:5000/api/auth/exists/email/' + email);
+export const checkUsernameExists = (username) => axios.get('/:5000/api/auth/exists/username/' + username);
 
-export const localRegister = ({email,username,password}) => axios.post('/api/auth/register/local',{email,username,password});
-export const localLogin = ({email,password}) => axios.post('/api/auth/Login/local',{email,password});
+export const localRegister = ({email,username,password}) => axios.post('/:5000//api/auth/register/local',{email,username,password});
+export const localLogin = ({email,password}) => axios.post('/:5000//api/auth/Login/local',{email,password});
 
 export const checkStatus = () => axios.get('/api/auth/check');
 export const logout = () => axios.post('/api/auth/logout');
