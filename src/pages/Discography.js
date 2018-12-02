4d90873ecda488a42663e74e0a63e0ecd92e6319
weaklion <weaklion@naver.com>
2018-12-02 16:46:25 +0900

    node commit

--- src/pages/Discography.js
+++ src/pages/Discography.js
@@ -1,7 +1,22 @@
 import React,{Component} from 'react';
 import Header from '../components/common/Header';
-import  './Discography.css';
 import Disco from '../components/Disco';
+import styled from 'styled-components';
+
+
+const Background=styled.div`
+    min-height:100%;
+    width:100%;
+    background:url('../image/4.jpg');
+    background-repeat: no-repeat;
+     background-size:cover;
+     background-position:center;
+     background-attachment:fixed;  
+     -moz-background-size: cover;
+-o-background-size: cover;
+-webkit-background-size: cover;
+`;
+
 
 
 
@@ -9,10 +24,10 @@ class Discography extends Component {
     render(){
     return(
       
-   <div className="Background">
+   <Background>
         <Header/>
         <Disco/>
-    </div>
+    </Background>
    
     )
 };
